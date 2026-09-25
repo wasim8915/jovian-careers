744b656c19a3b73d43268c62145c63695c15bb85
wasim ahmed <c_wasim.a@ults.in>
2026-09-25 20:56:16 +0530

    test

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({"message": "Hello, Worlddddd!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
