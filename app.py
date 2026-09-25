948525d65322d075223fd322796b998b1a0b84d4
wasim8915 <wasim8915@gmail.com>
2026-09-25 21:34:01 +0530

    sadf

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"message": "Hellggggo, Worlddddd!"})
+    return jsonify({"message": "Hellggggodfsdafdfd, Worlddddd!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
