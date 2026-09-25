b4a62d0d0ff17116dc7b3f41ba92bc967a1a45a2
wasim8915 <wasim8915@gmail.com>
2026-09-25 22:11:49 +0530

    gsdfgs

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
+Jobs = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bangalore, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Bangalore, India',
+        'salary': 'Rs. 15,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Developer',
+        'location': 'Remote',
+        'salary': '$120,000'
+    },
+    {
+        'id': 4,
+        'title': 'Backend Developer',
+        'location': 'New York, USA',
+        'salary': '$150,000'
+    }
+]
+
 @app.route('/')
 def index():
-    return jsonify({"message": "Hellggggodfsdafdfd, Worlddddd!"})
+    return render_template('home.html', jobs=Jobs)
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(Jobs)
 
 if __name__ == '__main__':
     app.run(debug=True)
